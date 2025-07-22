e9e425bad45131c9c105cfd4c39d5eb276e54667
rogerin07 <rogersantosbezerra@gmail.com>
2025-07-22 17:22:34 -0300

    first commit

---
+++ backend/userSeed.js
@@ -0,0 +1,14 @@
+import User from "./models/User"
+
+const userRegister = async () => {
+    try {
+        const newUser = new User({
+            name: 
+        })
+    } catch () {
+        
+    }
+
+}
+
+export default userSeed
---
+++ frontend/src/App.jsx
@@ -0,0 +1,5 @@
+function App() {
+  return <div className="text-3xl text-cyan-500">Hello world</div>;
+}
+
+export default App;
